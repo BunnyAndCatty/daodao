f2dfadbeccdd169a417c080fe44cf3291eaa9a55
赵愉 <yuzhao214960@souhu-inc.com>
2018-11-08 15:34:03 +0800

    tag-detail

--- src/pages/record/index.vue
+++ src/pages/record/index.vue
@@ -8,24 +8,37 @@
     <div v-else>
       <div class="header">
         <div class="type">
-          <span @click="select_pay" :class="{activeType: activeType===0}">支出</span>
-          <span @click="select_income" :class="{activeType: activeType===1}">收入</span>
+          <span @click="selectPay" :class="{activeType: activeType===0}">支出</span>
+          <span @click="selectIncome" :class="{activeType: activeType===1}">收入</span>
         </div>
         <input class="number" v-model="number"></input>
       </div>
       <div class="tags">
-        <scroll-view scroll-x="true" class="scroll-view_H">
-          <div class="scroll-view_H_item" v-for="(item, index) in icon_container_item_array">
-            <div class="iconContainer">
-              <div class="icon" v-for="(icon_item, icon_index) in item">
-                <div :class="['iconfont', 'icon_img', 'icon-'+icon_item.icon]"></div>
-                <div class="icon_desc">{{icon_item.desc}}</div>
+        <swiper class="swiper" indicator-dots="true">
+          <div class="swiper_item" v-for="(item, index) in icon_container_item_array" :key="index">
+            <swiper-item>
+              <div class="iconContainer">
+                <div class="icon" v-for="(icon_item, icon_index) in item" @click="showTagDetail(index, icon_index)">
+                  <div :class="['iconfont', 'icon_img', 'icon-'+icon_item.icon]"></div>
+                  <div class="icon_desc">{{icon_item.desc}}</div>
+                </div>
               </div>
-            </div>
+            </swiper-item>
           </div>
-        </scroll-view>
+        </swiper>
       </div>
-      <numberKeyBoard @clickKeyboard="clickKeyboard"></numberKeyBoard>
+      <div class="tagDetail" v-if="show_tag_detail" @click="closeTagDetail">
+        <radio-group class="radio-group" @change="radioChange" color="yellow">
+          <label class="radio_item" v-for="(item,index) in tag_detail" :key="index" @click="choseTag(item)">
+            <span class="radio_left">
+                <div :class="['iconfont', 'icon_img', 'icon-'+item.icon]"></div>
+                <div class="icon_desc">{{item.desc}}</div>
+            </span>
+            <radio :value="item.desc" :checked="item.checked" class="radio_circle" color="rgb(255,204,0)"/>
+          </label>
+        </radio-group>
+      </div>
+      <numberKeyBoard @clickKeyboard="clickKeyboard"  v-if="!show_tag_detail"></numberKeyBoard>
     </div>
   </div>
 </template>
@@ -44,10 +57,24 @@ export default {
       already_get_setting: '',
       activeType: 0,
       number: '0.00',
-      icon_array: [
+      tags_array: [
         {
           icon: 'zuofan',
-          desc: '餐饮'
+          desc: '餐饮',
+          items: [
+            {
+              icon: 'mianbao',
+              desc: '早餐'
+            },
+            {
+              icon: 'jitui',
+              desc: '午餐'
+            },
+            {
+              icon: 'mifan',
+              desc: '晚餐'
+            }
+          ]
         },
         {
           icon: 'gouwuche',
@@ -93,15 +120,24 @@ export default {
           icon: 'wenhao',
           desc: '其他'
         }
-      ]
+      ],
+      show_tag_detail: false,
+      tag_detail: [] // 子标签，详情dialog
     }
   },
 
   computed: {
     icon_container_item_array: function () {
       let array = []
-      for (let i = 0; i < this.icon_array.length; i = i + 8) {
-        array.push(this.icon_array.slice(i, i + 8))
+      for (let i = 0; i < this.tags_array.length; i = i + 8) {
+        if (this.tags_array[i].items) {
+          this.tags_array[i].items.unshift({
+            'icon': this.tags_array[i].icon,
+            'desc': this.tags_array[i].desc,
+            'checked': true
+          })
+        }
+        array.push(this.tags_array.slice(i, i + 8))
       }
       return array
     }
@@ -118,10 +154,10 @@ export default {
           })
       }
     },
-    select_pay () {
+    selectPay () {
       this.activeType = 0
     },
-    select_income () {
+    selectIncome () {
       this.activeType = 1
     },
     clickKeyboard (result) {
@@ -131,10 +167,31 @@ export default {
       } else {
         this.number = 0.00
       }
+    },
+    // tag详情
+    showTagDetail (index, iconIndex) {
+      let tagDetail = this.icon_container_item_array[index][iconIndex]
+      if (tagDetail.items && tagDetail.items.length > 0) {
+        this.show_tag_detail = true
+        this.tag_detail = tagDetail.items
+      }
+    },
+    radioChange (e) {
+      // console.log(e.mp.detail.value)
+    },
+    closeTagDetail () {
+      this.show_tag_detail = false
+    },
+    choseTag (item) {
+      this.tag_detail.forEach(element => {
+        element.checked = false
+      })
+      item.checked = true
+      console.log(item)
     }
   },
   mounted () {
-    console.log('mounted')
+    // console.log('mounted')
   },
   onShow () {
     wx.setNavigationBarTitle({
@@ -206,14 +263,14 @@ export default {
     }
   }
   .tags{
-    .scroll-view_H{
-      white-space: nowrap;
-      .scroll-view_H_item{
+    .swiper{
+      height: 170px;
+      padding: 10px 0;
+      .swiper_item{
         width: 100%;
-        display: inline-block;
         .iconContainer{
           width: 100%;
-          height: 130px;
+          height: 140px;
           display: flex;
           flex-wrap: wrap;
           justify-content: space-around; 
@@ -226,10 +283,56 @@ export default {
             .icon_desc{
               font-size: 12px;
               text-align: center;
+              margin-top: 5px;
             }
           }
           
         }
+      }
+    }
+  }
+  .tagDetail{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(85, 85, 102, 0.5);
+    .radio-group{
+      width: 100%;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      background-color: #ffffff;
+      opacity: 1;
+      border-top-left-radius: 10px;
+      border-top-right-radius: 10px;
+      .radio_item{
+        padding: 5px 15px;
+        border-top: 1px solid rgba(85, 85, 102, 0.5);
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .radio_left{
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          .icon_img{
+            font-size: 30px;
+          }
+          .icon_desc{
+            font-size: 12px;
+            text-align: center;
+            margin-left: 20px;
+          }
+        }
+        .radio_circle{
+
+        }
+      }
+      .radio_item:nth-child(1){
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
       }
     }
   }
